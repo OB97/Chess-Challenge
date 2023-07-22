2b1a302966292f6bec9f45fe2087fe0372f57d2a
Alex <ajobrien@upei.ca>
2023-07-22 14:54:46 -0300

    Update MyBot.cs
    syncing brock

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -5,6 +5,7 @@ public class MyBot : IChessBot
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
+
         System.Random rand = new();
         return moves[rand.Next(moves.Length)];
     }
