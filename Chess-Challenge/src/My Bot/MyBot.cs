eba3570a50f31db4bf0229e5003f6300dc055877
Alex <ajobrien@upei.ca>
2023-07-24 09:19:05 -0300

    Updated

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,12 @@
 ﻿using ChessChallenge.API;
+using System.Diagnostics;
 
 public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
+        Debug.WriteLine(moves[0]);
         System.Random rand = new();
         return moves[rand.Next(moves.Length)];
     }
